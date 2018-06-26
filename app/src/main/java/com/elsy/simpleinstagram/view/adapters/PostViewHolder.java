74732d2d677fbd52fe901ca1a87325afc055e33e
Elsy Pinzon <elsy.pinzon@wizeline.com>
2018-06-26 10:23:40 -0500

    Delete unnecesary scope modifier

--- app/src/main/java/com/elsy/simpleinstagram/view/adapters/PostViewHolder.java
+++ app/src/main/java/com/elsy/simpleinstagram/view/adapters/PostViewHolder.java
@@ -12,9 +12,9 @@ public class PostViewHolder extends RecyclerView.ViewHolder {
         ImageView postPicture;
         TextView postTitle;
 
-        public PostViewHolder(View view) {
+         PostViewHolder(View view) {
             super(view);
-            postPicture = (ImageView) view.findViewById(R.id.picture);
-            postTitle = (TextView) view.findViewById(R.id.title);
+            postPicture = view.findViewById(R.id.picture);
+            postTitle = view.findViewById(R.id.title);
         }
 }
