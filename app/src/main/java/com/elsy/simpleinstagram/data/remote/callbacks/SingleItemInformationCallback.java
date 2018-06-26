cf1d2d75bdceae70eedd6745ca61e41bfe6ac55d
Elsy Pinzon <elsy.pinzon@wizeline.com>
2018-06-26 12:39:19 -0500

    Delete unused interface

--- app/src/main/java/com/elsy/simpleinstagram/data/remote/callbacks/SingleItemInformationCallback.java
+++
@@ -1,9 +0,0 @@
-package com.elsy.simpleinstagram.data.remote.callbacks;
-
-import com.elsy.simpleinstagram.data.remote.callbacks.ServerCallback;
-
-public interface SingleItemInformationCallback<Item> extends ServerCallback {
-
-    void onItemInformationLoaded(Item item);
-
-}
