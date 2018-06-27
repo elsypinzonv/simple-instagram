dd413c3c9a716b2923ab2cb8144e52d51d79066e
Elsy Pinzon <elsy.pinzon@wizeline.com>
2018-06-26 23:54:38 -0500

    Add unitTest for hostname validation

---
+++ app/src/test/java/com/elsy/simpleinstagram/ValidationsUnitTest.java
@@ -0,0 +1,26 @@
+package com.elsy.simpleinstagram;
+
+import com.elsy.simpleinstagram.utils.ActivityHelper;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ValidationsUnitTest {
+
+    @Test
+    public void invalidHostname_isCorrect() {
+        assertEquals(false, ActivityHelper.isValidHostame("google.com"));
+    }
+
+    @Test
+    public void validHostname_isCorrect() {
+        assertEquals(true, ActivityHelper.isValidHostame("photomaton.herokuapp.com"));
+    }
+
+}
